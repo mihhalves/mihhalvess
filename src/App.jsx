c8c8926bee724c11341dabddabd51feb1ae38488
Michelle <michelle.selva@gmail.com>
2024-11-02 00:37:51 -0300

    add section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import "./app.scss"
 import Raimundas from "./components/raimundas/Raimundas"
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
@@ -10,10 +11,11 @@ const App = () => {
       <Navbar/>
       <Raimundas/>
       </section>
-    <section id="Sobrenos"><Parallax type="sobrenos"/></section>
+      <section id="Services"><Parallax type="services"/></section>
+      <section> <Services/> </section>
+    <section id="Portifolio"><Parallax type="portifolio"/></section>
     <section> Sobre nós </section>
-    <section id="servicos"><Parallax type="servicos"/></section>
-    <section id="Serviços"> Serviços </section>
+   
     <section id="Contato"> Contato</section>
      
   </div>;
